01b1603f2f8d182f8fa9bc8a3e7cb4acabcbaeb4
GirinoX45 <lubuntupython@gmail.com>
2026-03-09 19:24:02 +0000

    chore(downloader): configuração inicial do módulo **downloader.py** adicionada

---
+++ downloader.py
@@ -0,0 +1,49 @@
+import requests
+from abc import ABC, abstractmethod
+
+
+#Classes
+
+class Downloader(ABC):
+
+    @abstractmethod
+    def __init__(self,url:str):
+        pass
+
+    @abstractmethod
+    def baixar(self):
+        pass
+
+
+class DowloaderImagem(Downloader):
+    
+    def __init__(self, url):
+
+        if not isinstance(url,str):
+            raise ValueError(f"O valor do atributo tem que ser uma string!")
+        
+        if not "https" or "http" in url:
+            self.url = "https://" + url
+        
+        self.url = url
+    
+    def baixar(self):
+        
+        resp = requests.get(self.url)
+
+        with open("imagem_anime.jpg", "wb") as img:
+            img.write(resp.content)
+
+
+# Função Main
+
+def main():
+    
+    url = ""
+
+    d = DowloaderImagem(url)
+    d.baixar()
+
+
+if __name__ == "__main__":
+    main()
